5fce978afc3a2a47af932e874f04ca981664d4c0
Olle7 <olger1997@gmail.com>
2020-04-15 08:09:12 +0300

    Update Sorter5.java
    made some variables final.

--- Sorter5.java
+++ Sorter5.java
@@ -7,29 +7,16 @@ public class Sorter5 {
     public static void main(String[] args) throws IOException{
         long startTime = System.currentTimeMillis();
 
-        BitSet existing_numbers = new BitSet(10000000);//9000000//26
+        BitSet existing_numbers = new BitSet(10000000);
         FileInputStream inputFile = new FileInputStream(args[0]);
         final int lines_in_file=9000000;
-
-        int lines_in_buffer=6000;
-        //50:1742*ms
-        //100:1634*ms
-        //130:1558*ms
-        //140:1547*ms
-        //150:1604*ms
-        //160:1619*ms
-        //200:2497*ms
-        //1000:1922*ms
-        //5000:1452*ms
-        //20000:1449*ms
-        //50000:1514*ms
-        int pages=lines_in_file/lines_in_buffer;
+        final int lines_in_buffer=6000;
 
         byte[] buffer=new byte[lines_in_buffer*8];
-        for (int page_number=0;page_number<pages;page_number++){
+        for (int page_number=0;page_number<lines_in_file/lines_in_buffer;page_number++){
             inputFile.read(buffer);
-            for (int line_number=0;line_number<lines_in_buffer;line_number++){
-                existing_numbers.set((buffer[line_number*8]-'0')*1000000+(buffer[line_number*8+1]-'0')*100000+(buffer[line_number*8+2]-'0')*10000+(buffer[line_number*8+3]-'0')*1000+(buffer[line_number*8+4]-'0')*100+(buffer[line_number*8+5]-'0')*10+(buffer[line_number*8+6]-'0'));
+            for (int character_number=0;character_number<lines_in_buffer*8;character_number+=8){
+                existing_numbers.set((buffer[character_number]-'0')*1000000+(buffer[character_number+1]-'0')*100000+(buffer[character_number+2]-'0')*10000+(buffer[character_number+3]-'0')*1000+(buffer[character_number+4]-'0')*100+(buffer[character_number+5]-'0')*10+(buffer[character_number+6]-'0'));
             }
         }
         inputFile.close();
